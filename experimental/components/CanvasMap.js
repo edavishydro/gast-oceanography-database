f443e28ed437781d3a13cbef27f20c86a4bef632
Edward Davis <hello@edwdavis.com>
2020-03-30 16:19:01 -0700

    add functional files from MergeAttempt branch

--- experimental/components/CanvasMap.js
+++
@@ -1,7 +0,0 @@
-import CMBase from "../Libraries/CanvasMap_11_27_19";
-
-const MapMaker = () => {
-  return <div>Help me</div>;
-};
-
-export default MapMaker;
--- experimental/pages/index.js
+++
@@ -1,9 +0,0 @@
-import Layout from "../components/Layout";
-
-const Index = () => (
-  <Layout>
-    <div>Hello Next.js</div>
-  </Layout>
-);
-
-export default Index;
