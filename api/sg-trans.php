63bbc7f0ea8584f2e78ed99d01c4cd182be8082d
Edward Davis <hello@edwdavis.com>
2020-03-31 12:15:36 -0700

    enable drive api on submit

--- api/sg-trans.php
+++ api/sg-trans.php
@@ -1,26 +1,44 @@
 <?php
   require_once __DIR__.'/phpdotenv/vendor/autoload.php';
   require_once __DIR__.'/sendgrid-php/vendor/autoload.php';
+  require_once __DIR__.'/google-api-php-client/vendor/autoload.php';
 
   $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../../etc', 'secrets.env');
   $dotenv->load();
 
   if(isset($_POST['request'])) {
     $json = $_POST['request'];
-    //var_dump(json_decode($json, true));
-    //$file = fopen('test.json','w');
-    //fwrite($file, $json);
-    //fclose($file);
-    $request = json_decode($json);
-    $userFullName = $request->user->fullName;
-    $userMail = $request->user->email;
-    $userAffiliation = $request->user->affiliation;
-    $documents = $request->documents;
-    $docTable = array();
-    foreach ($documents as $doc) {
-      $docTable[] = "<tr><td>".$doc->id."</td><td>".$doc->Title."</td><td>".$doc->Author."</td></tr>";
+
+    $client = new Google_Client();
+    if (getenv('GOOGLE_APPLICATION_CREDENTIALS')) {
+      // use the application default credentials
+      $client->useApplicationDefaultCredentials();
+    } else {
+      echo missingServiceAccountDetailsWarning();
+      return;
     }
+
+    $client->setApplicationName("Gast_Oceanography_Collection");
+    $client->setScopes(['https://www.googleapis.com/auth/drive']);
+    
+    $service = new Google_Service_Drive($client);
+    $folder_id = "1uv4ZT37Nxi_6FoChjeiQxqbjWqS51pHW";
+    $rand_num = mt_rand(1000, 9999);
+
+    $fileMetadata = new Google_Service_Drive_DriveFile(array(
+      'name' => 'request'.($rand_num).'.json',
+      'parents' => array($folder_id)));
+
+    $content = $json;
+  
+    $file = $service->files->create($fileMetadata, array(
+      'data' => $content,
+      'mimeType' => 'application/json',
+      'uploadType' => 'multipart',
+      'fields' => 'id'));
+
     $sendgridData = json_decode($json, true);
+    $sendgridData += array('link' => 'https://drive.google.com/open?id='.$file->id);
 
     $email = new \SendGrid\Mail\Mail();
 
@@ -29,7 +47,7 @@
 
     $email->addTo(
         'edavis@tgaec.com',
-        $userFullName,
+        'Gast Oceanography Collection',
         $sendgridData,
         0
     );
@@ -37,7 +55,7 @@
     $email->setTemplateId("d-3a9d6efd7dc0418ca6aef8b71e2c98de");
 
     $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
-
+    
     try {
         $response = $sendgrid->send($email);
         print $response->statusCode() . "\n";
